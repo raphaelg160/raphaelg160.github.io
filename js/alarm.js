795bb1a42fdd6720c8dfaa561d70fafd7129f7c2
Use your GitHub username <Enter your email address>
2020-11-07 23:36:57 -0300

    last update

--- js/alarm.js
+++ js/alarm.js
@@ -1,4 +1,4 @@
-function myDate() {
+function Date() {
     // Input 
     // Insert number information to determine if can sleep in or not
     let now = new Date();
@@ -8,9 +8,9 @@ function myDate() {
     // Processing
     // Provide the sleep in or not message
     let = message;
-    if ((month === "0" && dayOfMonth === "1") || (month === "7" && dayOfMonth === "4") || (month === "11" && dayOfMonth === "25")) {
+    if ((month === 0 && dayOfMonth === 1) || (month === 7 && dayOfMonth === 4) || (month === 11 && dayOfMonth === 25)) {
         message = "Sleep in."
-    } else if ((dayOfWeek === "0" && dayOfWeek === "6")) {
+    } else if ((dayOfWeek === 0 && dayOfWeek === 6)) {
         message - "Sleep in."
     }    else {
         message = "Get up!"
